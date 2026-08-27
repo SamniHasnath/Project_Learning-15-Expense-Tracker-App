ff630d5813083ec3274851970c03b915db3318de
SamniHasnath <samnihasnath@gmail.com>
2026-08-27 10:13:22 +0530

    Update database configuration

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+const isLocal = /localhost|127\.0\.0\.1/.test(process.env.DB_URL || "");
+
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: isLocal ? false : { rejectUnauthorized: false },
+  connectionTimeoutMillis: 10000,
 });
 
 module.exports = pool;
